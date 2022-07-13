5bf26097869a8f7d55fc8292a340e8465f51a54c
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-07-13 15:23:55 +0700

    Stage 2/5

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        char[] game = scanner.next().toCharArray();
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++){
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++){
+                System.out.printf("%c ", game[i * 3 + j]);
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
